e03369c2bda80eba5dfcf85ee526620bc59163c8
Tkunsss <sotikun6@gmail.com>
2026-02-12 09:04:21 +0700

    add quiz

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -20,6 +20,8 @@ function App() {
   }
   function increasePrice() {
     // TODO Update the value in dollars by increasing it by 5
+    setValueDollars(valueDollars + 5);
+    return valueDollars;
   }
   return (
     <main>
@@ -35,7 +37,7 @@ function App() {
         <label>Value in Euro</label>
         <input disabled value={dollarToEuro(valueDollars)} />
         {/* TODO */}
-        <button>Increase Price + 5</button>
+        <button onClick={increasePrice}>Increase Price + 5</button>
 
       </p>
     </main>
